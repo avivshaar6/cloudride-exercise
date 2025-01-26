d0682a8343a92cd62301bf1db5210e8815d1303c
avivshaar6 <avivshaar6@gmail.com>
2025-01-26 21:35:03 +0200

    remove tag from tfvars, add targets in apply

--- src/app.py
+++ src/app.py
@@ -1,22 +1,29 @@
-from flask import Flask
+from flask import (
+    Flask,
+    jsonify
+)
 import os
 import logging
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 APP_PORT = int(os.environ.get('APP_PORT', 8000))
+APP_VERSION = os.environ.get('APP_VERSION', 'latest')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     logger.info('Hello World endpoint was called')
-    # return 'Hello, World!'
-    return 'Hello, World! 11'
+    
+    return jsonify({
+        "status": "healthy",
+        "message": "Hello, World!",
+        "version": APP_VERSION
+    })
 
 if __name__ == '__main__':
     logger.info(f'Starting application on port {APP_PORT}')
     app.run(host='0.0.0.0', port=APP_PORT, debug=False)
-
-
